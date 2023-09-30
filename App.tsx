653551547636fe12ef811a70200d254c0ef2ea3d
eirikhanasand <eirik.hanasand@gmail.com>
2023-09-30 03:28:06 +0200

    Added redux

--- App.tsx
+++ App.tsx
@@ -1,14 +1,26 @@
 import Navigator from "@nav/tabs"
+import { NavigationContainer } from "@react-navigation/native"
+import { PersistGate } from "redux-persist/integration/react"
+import { persistStore } from "redux-persist"
+import { Provider } from "react-redux"
+import store from "@redux/store"
+import React from "react"
+
+let persistor = persistStore(store)
 
 /**
  * Main entry point of the application
  * 
- * Almost empty for now, I prefer only having navigation and redux in this file.
- * 
- * @returns 
+ * @returns Entire application as a JSX Component
  */
-function App(): JSX.Element {
-    return <Navigator/>
+export default function App(): JSX.Element {
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <NavigationContainer>
+                    <Navigator/>
+                </NavigationContainer>
+            </PersistGate>
+        </Provider>
+    )
 }
-
-export default App
